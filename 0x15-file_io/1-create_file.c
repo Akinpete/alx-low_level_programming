86621f5208eee848ed44a9d5f0b4dd2f656ddd0c
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2023-05-27 06:32:56 -0700

    betty fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,53 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include "main.h"
+#include <string.h>
 
 /**
  * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * @filename: name of file to be created
+ * @text_context: text to be written to file
  *
- * Return: 1 if it succeeds. -1 if it fails.
+ * Return: 1 or -1
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_context)
 {
-	int fd;
-	int nletters = 0;
-	int rwr;
+	FILE *fp; /* pointer to a FILE object */
+	mode_t mode; /* sets permissions to a file */
+	int write_check;
+	int chmod_check;
 
-	if (filename == NULL)
-		return (-1);
+	if (access(filename, F_OK) == 0) /* if file exists) */
+	{
+		fp = fopen(filename, "w"); /* open file in write mode, overwrite */
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fp == NULL || filename == NULL)
+			return (-1);
+		if (text_context != NULL)
+			write_check = fwrite(text_context, sizeof(char), strlen(text_context), fp);
+		if (write_check == -1) /* write unsuccessful */
+			return (-1);
+		fclose(fp); /* close file */
+		return (1);
+	}
+	else /* File doesn't exists */
+	{
+		fp = fopen(filename, "w"); /* open file in write mode */
+		if (fp == NULL || filename == NULL)
+			return (-1);
+		if (text_context != NULL)
+			write_check = fwrite(text_context, sizeof(char), strlen(text_context), fp);
+		if (write_check == -1) /* write unsuccessful */
+			return (-1);
+		fclose(fp); /* close file */
 
-	if (fd < 0)
-		return (-1);
+		mode = S_IRUSR | S_IWUSR; /* specify permissions */
+		chmod_check = chmod(filename, mode);
 
-	while (text_content && *(text_content + nletters)) 
-		nletters++;
-
-	rwr = write(fd, text_content, nletters);
-	
-	close(fd);
-
-	if (rwr < 0)
-		return (-1);
-	return (1);
+		if (chmod_check != 0) /* permission change failed */
+			return (0);
+		return (1);
+	}
 }
