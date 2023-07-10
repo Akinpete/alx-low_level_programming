dd9114fa95ef4dca2044d122b0359c363fd0f1e0
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2023-07-10 21:31:48 +0100

    betty fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	while (text_content && *(text_content + nletters)) 
+	while (text_content && *(text_content + nletters))
 		nletters++;
 
 	rwr = write(fd, text_content, nletters);
-	
+
 	close(fd);
 
 	if (rwr < 0)
